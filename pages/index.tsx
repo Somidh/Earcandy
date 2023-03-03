58370ee84aeaa4914fa3be17d4ab68db565abd98
Sagar Bera <contact.sagarbera@gmail.com>
2023-03-04 03:20:40 +0530

    chore: added folder structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <h1 className="text-blue-400 bg-red-200">dasflaks</h1>;
+  return <h1 className="fg bg-red-200 text-blue-400">dasflaks</h1>;
 };
 
 export default Home;
