935ddf00af85c47265f8206fdc94d9fe57fc3a97
Sagar Bera <contact.sagarbera@gmail.com>
2023-03-04 03:17:50 +0530

    style: defined tailwind defaults

---
+++ tailwind.config.cjs
@@ -0,0 +1,44 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E5EDE8",
+        accent: "#C6DBCE",
+        btn: "#303933",
+      },
+      textColor: {
+        skin: {
+          base: "#000000",
+        },
+      },
+      backgroundColor: {
+        card: "#27312B",
+      },
+      fontSize: {
+        xs: "12px",
+        xss: "10px",
+      },
+      gridTemplateColumns: {
+        14: "repeat(14, minmax(0, 1fr))",
+        16: "repeat(16, minmax(0, 1fr))",
+      },
+      fontFamily: {
+        noto: ["Noto Serif", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("prettier-plugin-tailwindcss"),
+  ],
+};
