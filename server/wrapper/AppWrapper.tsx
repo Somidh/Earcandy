973a73544cc0c63ad645a5a8a75de8259cb4d06b
aadarsh805 <aadarshthakur80501@gmail.com>
2023-03-04 16:46:54 +0530

    feat: navbar state change on login

--- server/wrapper/AppWrapper.tsx
+++ server/wrapper/AppWrapper.tsx
@@ -1,4 +1,6 @@
 import createUser from "@/server/lib/createUser";
+import useStore from "@/store/store";
+import { IUser } from "@/types/TUser";
 import { UserProfile, useUser } from "@auth0/nextjs-auth0/client";
 import { FC, ReactNode, useEffect } from "react";
 import supabase from "../supabase";
@@ -9,6 +11,11 @@ type TAppWrapper = {
 
 const AppWrapper: FC<TAppWrapper> = ({ children }) => {
   const { user, isLoading } = useUser();
+  const { setUserProfile } = useStore((state) => {
+    return {
+      setUserProfile: state.setUserProfile,
+    };
+  });
 
   const handleUserExists = async () => {
     const { data, error } = await supabase
@@ -19,10 +26,12 @@ const AppWrapper: FC<TAppWrapper> = ({ children }) => {
     if (data?.[0]?.email) {
       console.log("user already exits");
       console.log(data?.[0]);
+      setUserProfile(data?.[0] as IUser);
       return;
     } else {
       const newUser = await createUser(user as UserProfile);
       console.log(newUser, "newuser");
+      setUserProfile(newUser as IUser);
     }
   };
 
